1ea4975cfb21104b172c9d3c5fb76f8d9711501f
NekronisXIII <chrisi184@t-online.de>
2024-09-17 12:39:35 +0200

    Implemented options open and close behavior.

--- MVVM/ViewModels/MainWindowViewModel.cs
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LF10_Project.MVVM.ViewModels
 {
-	internal class MainWindowViewModel
+	internal partial class MainWindowViewModel : ObservableObject
 	{
-	}
+        #region Members
+        [ObservableProperty]
+        private bool _optionsOpen;
+        #endregion
+
+        #region Methods
+        #region Private methods
+
+        [RelayCommand]
+        private void OpenOptions()
+        {
+            OptionsOpen = true;
+        }
+
+        [RelayCommand]
+        private void CloseOptions()
+        {
+            OptionsOpen = false;
+        }
+        #endregion
+        #endregion
+    }
 }
