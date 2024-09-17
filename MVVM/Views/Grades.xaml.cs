d20903841648543d911bbaa5cb14480ee2048438
Tobias Grah <tobias.grah@cirromed.de>
2024-09-17 10:24:35 +0200

    Creaeted Grates view

---
+++ MVVM/Views/Grades.xaml.cs
@@ -0,0 +1,27 @@
+﻿using LF10_Project.MVVM.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LF10_Project.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Grades : Window
+    {
+        public Grades()
+        {
+
+			InitializeComponent();
+        }
+    }
+}
